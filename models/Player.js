b65524fd5c6b20a25f1e13126337cc10c273c718
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2018-09-23 21:45:05 +0200

    Player receives mongoDB properties

--- models/Player.js
+++ models/Player.js
@@ -2,22 +2,28 @@ var mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
 
 var PlayerSchema = new mongoose.Schema({
+    id: { type: mongoose.Schema.Types.ObjectId },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     gameId: { type: mongoose.Schema.Types.ObjectId, ref: 'Game', required: [true, "can't be blank"] },
     username: { type: mongoose.Schema.Types.String, required: [true, "can't be blank"] },
     buyIns: { type: [] },
-    active: { type: mongoose.Schema.Types.Boolean, required: [true, "can't be blank"] }
+    active: { type: mongoose.Schema.Types.Boolean, required: [true, "can't be blank"] },
+    createdAt: { type: mongoose.Schema.Types.Date },
+    updatedAt: { type: mongoose.Schema.Types.Date },
 }, {timestamps: true});
 
 PlayerSchema.plugin(uniqueValidator, {message: 'is already taken.'});
 
 PlayerSchema.methods.toJSON = function(){
   return {
+    id: this._id,
     userId: this.userId,
     gameId: this.gameId,
     username: this.username,
     buyIns: this.buyIns,
-    active: this.active
+    active: this.active,
+    createdAt: this.createdAt,
+    updatedAt: this.updatedAt,
   };
 };
 
